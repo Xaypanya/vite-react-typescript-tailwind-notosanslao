5d5b51bba13e1340bd513dd06e5e2bd026b628a4
Xaypanya Phongsa <dackogan@gmail.com>
2023-01-31 15:05:38 +0700

    initial project v1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App font-sans">
+          <div className='w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-blue-600 relative'>
+                <h1 className='text-5xl text-white drop-shadow-xl mb-5'>ສະບາຍດີ</h1>
+                <h1 className='text-xl text-white drop-shadow-xl font-pops'>React + TypeScript + TailwindCSS</h1>
+          </div>
+    </div>
+  )
+}
+
+export default App
